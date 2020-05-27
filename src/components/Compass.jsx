f731cb41cc1cae1fbd6f47a8be9fa8f4f6f9359e
James Oliver <joliver.robert@me.com>
2020-05-27 07:52:32 +0100

    created Compass component

---
+++ src/components/Compass.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Compass = () => {
+  return (
+    <img
+      src="/images/compass.png"
+      alt="discover topics"
+      height="30px"
+      width="30px"
+    />
+  );
+};
+
+export default Compass;
