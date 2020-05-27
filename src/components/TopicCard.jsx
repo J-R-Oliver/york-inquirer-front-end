8232ff995cf2a3ad3ff5d4f0069b1e4f6d256c6f
James Oliver <joliver.robert@me.com>
2020-05-27 10:57:14 +0100

    updated TopicCard with link to ArticleList filtered by topic

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import Title from './Title';
 import Text from './Text';
 
 const TopicCard = ({ slug, description }) => {
   return (
     <article>
-      <Title>{`#${slug}`}</Title>
+      <Link to={`/${slug}`}>
+        <Title>{`#${slug}`}</Title>
+      </Link>
       <Text>{description}</Text>
     </article>
   );
