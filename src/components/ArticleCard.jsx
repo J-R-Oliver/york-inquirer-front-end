66db7ab986bd77fe2c508256f80a110ee38364f5
James Oliver <joliver.robert@me.com>
2020-05-27 11:01:46 +0100

    updated ArticleCard with link to ArticleList filtered by topic

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import * as formatDate from '../utils/formatDate';
 import Text from './Text';
 import Title from './Title';
 import Votes from './Votes';
 import Comments from './Comments';
 
-const ArticleCard = ({ author, created_at, title, votes, comment_count }) => {
+const ArticleCard = ({
+  author,
+  created_at,
+  title,
+  votes,
+  comment_count,
+  topic
+}) => {
   const createdDate = formatDate(created_at);
 
   return (
@@ -17,6 +25,9 @@ const ArticleCard = ({ author, created_at, title, votes, comment_count }) => {
         <Votes votes={votes} />
         <Comments comment_count={comment_count} />
       </Text>
+      <Link to={`/${topic}`}>
+        <Text>{`#${topic}`}</Text>
+      </Link>
     </article>
   );
 };
