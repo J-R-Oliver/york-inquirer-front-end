35a1a9448aedcd6522d0435cd74b7cc8a85f6160
James Oliver <joliver.robert@me.com>
2020-05-27 12:13:18 +0100

    created getArticle and getComments api request functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,6 +10,22 @@ export const getArticles = topic => {
     });
 };
 
+export const getArticle = article_id => {
+  return axios
+    .get(`${hostname}/api/articles/${article_id}`)
+    .then(({ data: { article } }) => {
+      return article;
+    });
+};
+
+export const getComments = article_id => {
+  return axios
+    .get(`${hostname}/api/articles/${article_id}/comments`)
+    .then(({ data: { comments } }) => {
+      return comments;
+    });
+};
+
 export const getTopics = () => {
   return axios.get(`${hostname}/api/topics`).then(({ data: { topics } }) => {
     return topics;
