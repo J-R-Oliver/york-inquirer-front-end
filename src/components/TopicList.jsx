71541899d6a065538dbe6f27063f47378cf76aa7
James Oliver <joliver.robert@me.com>
2020-05-27 08:33:29 +0100

    created TopicList component

---
+++ src/components/TopicList.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { getTopics } from '../utils/api';
+import Loading from './Loading';
+import TopicCard from './TopicCard';
+
+class TopicList extends Component {
+  state = {
+    topics: [],
+    isLoading: true
+  };
+
+  componentDidMount() {
+    this.fetchTopics();
+  }
+
+  fetchTopics() {
+    getTopics().then(topics => {
+      this.setState({ topics, isLoading: false });
+    });
+  }
+
+  render() {
+    const { topics, isLoading } = this.state;
+
+    if (isLoading) return <Loading />;
+
+    return (
+      <main>
+        {topics.map(({ slug, description }) => {
+          return <TopicCard key={slug} slug={slug} description={description} />;
+        })}
+      </main>
+    );
+  }
+}
+
+export default TopicList;
