77260ba950e1f6c87ab869e45a32c0e4c92df2ef
James Oliver <joliver.robert@me.com>
2020-05-29 08:41:10 +0100

    created CustomError component

---
+++ src/components/CustomError.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import Text from './Text';
+import Title from './Title';
+
+const Error = ({ err }) => {
+  return (
+    <main>
+      <article>
+        <Title>
+          {err === 'Request failed with status code 400'
+            ? 'Oh No! 400 - Bad Request!'
+            : 'Oh No! 404 - Page not found!'}
+        </Title>
+        <Link to="/">
+          <Text>Why not check out some of our other context</Text>
+        </Link>
+      </article>
+    </main>
+  );
+};
+
+export default Error;
