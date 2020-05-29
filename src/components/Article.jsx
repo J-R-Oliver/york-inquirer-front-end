6bd90f62df95ec0a607e74e5347fa9f4ede8546c
James Oliver <joliver.robert@me.com>
2020-05-29 08:50:43 +0100

    updated Article with error handling

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,6 +3,8 @@ import { Link } from '@reach/router';
 import { getArticle } from '../utils/api';
 import * as formatDate from '../utils/formatDate';
 import Loading from './Loading';
+import CustomError from './CustomError';
+import RetryError from './RetryError';
 import Title from './Title';
 import Text from './Text';
 import Votes from './Votes';
@@ -11,7 +13,8 @@ import CommentList from './CommentList';
 class Article extends Component {
   state = {
     article: {},
-    isLoading: true
+    isLoading: true,
+    err: false
   };
 
   componentDidMount() {
@@ -21,21 +24,31 @@ class Article extends Component {
   fetchArticle = () => {
     const { article_id } = this.props;
 
-    getArticle(article_id).then(article => {
-      this.setState({ article, isLoading: false });
-    });
+    getArticle(article_id)
+      .then(article => {
+        this.setState({ article, isLoading: false, err: null });
+      })
+      .catch(({ message }) => {
+        this.setState({ isLoading: false, err: message });
+      });
   };
 
   render() {
     const {
       article: { title, author, created_at, topic, body, votes, article_id },
-      isLoading
+      isLoading,
+      err
     } = this.state;
     const { username } = this.props;
+    const { fetchArticle } = this;
 
     const createdDate = formatDate(created_at);
 
     if (isLoading) return <Loading />;
+    if (err !== 'Network Error' && err !== '') {
+      return <CustomError err={err} />;
+    }
+    if (err) return <RetryError err={err} retryFunction={fetchArticle} />;
 
     return (
       <main>
@@ -47,9 +60,7 @@ class Article extends Component {
             <Text>{`#${topic}`}</Text>
           </Link>
           <Text>{body}</Text>
-          <Text>
-            <Votes votes={votes} path="articles" objectId={article_id} />
-          </Text>
+          <Votes votes={votes} path="articles" objectId={article_id} />
         </article>
         <CommentList article_id={article_id} username={username} />
       </main>
