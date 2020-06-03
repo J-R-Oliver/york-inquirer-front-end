c2752674d9df343fcfa75a75b281b56cd501e91c
James Oliver <joliver.robert@me.com>
2020-06-03 08:27:58 +0100

    updated App.js imports, added styling and removed moved components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Router } from '@reach/router';
-import Header from './components/Header';
-import ArticleList from './components/ArticleList';
-import TopicList from './components/TopicList';
-import Article from './components/Article';
-import CustomError from './components/CustomError';
+import Header from './components/header/Header';
+import ArticleList from './components/article-list/ArticleList';
+import TopicList from './components/topic-list/TopicList';
+import Article from './components/article/Article';
+import CustomError from './components/reused-components/CustomError';
+import Footer from './components/footer/Footer';
+
+const StyledWrapper = styled.div`
+  display: grid;
+  min-height: 100vh;
+  background-color: rgb(250, 250, 250);
+  grid-template-areas: '. . .' '. main .' '. footer .';
+  grid-template-columns: minmax(30px, 1fr) minmax(300px, 1008px) minmax(30px, 1fr);
+  grid-template-rows: 50px auto 36px;
+`;
+
+const StyledMain = styled.main`
+  grid-area: main;
+`;
 
 class App extends Component {
   state = {
@@ -16,16 +32,19 @@ class App extends Component {
     const { username } = this.state;
 
     return (
-      <>
+      <StyledWrapper>
         <Header />
-        <Router>
-          <ArticleList path="/" />
-          <ArticleList path="/:topic_slug" />
-          <TopicList path="/explore" />
-          <Article path="/read/:article_id" username={username} />
-          <CustomError default />
-        </Router>
-      </>
+        <StyledMain>
+          <Router>
+            <ArticleList path="/" />
+            <ArticleList path="/:topic_slug" />
+            <TopicList path="/explore" />
+            <Article path="/read/:article_id" username={username} />
+            <CustomError default />
+          </Router>
+        </StyledMain>
+        <Footer />
+      </StyledWrapper>
     );
   }
 }
--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loading = () => {
-  return <img src="/images/rose.png" alt="yorkshire rose" />;
-};
-
-export default Loading;
